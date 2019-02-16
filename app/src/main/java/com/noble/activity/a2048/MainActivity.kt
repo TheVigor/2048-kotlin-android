7c858305d9c14f83a6a33d975d40bff941a9a7fc
TheVigor <igor.proskuryakov14@gmail.com>
2019-02-16 13:20:17 +0300

    init empty project

---
+++ app/src/main/java/com/noble/activity/a2048/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.noble.activity.a2048
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
